94b3fec145a0c888c88c4e4c06c06e5f9dfcdeea
Khawklong <potbunpa;@gmail.com>
2026-02-09 18:40:02 +0700

    Fix postgres connection using DATABASE_URL

--- api/server.js
+++ api/server.js
@@ -8,12 +8,13 @@ const { Pool } = require('pg');
 const app = express();
 
 // Database connection
+const { Pool } = require('pg');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
+
 
 // 2️⃣ CORS configuration
 const corsOptions = {
